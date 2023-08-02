ad279ee3cde5ee4a876a990a06d42d5b76d0f341
Krzysztof Kosobudzki <krzysztof@koia.consulting>
2023-08-02 23:46:24 +0200

    feat(level): set level in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,16 +6,35 @@ import { init, log } from 'nosy-logger';
 init({ apiKey: 'some fake api key' });
 
 export default function App() {
-  const send = () =>
+  const debug = () =>
+    log('hello from nosy logger debug example!')
+    .then(() => console.log('logged debug', 'debug'))
+    .catch(e => console.error(e));
+
+  const info = () =>
     log('hello from nosy logger example!')
-      .then(() => console.log('logged'))
+    .then(() => console.log('logged info'))
+    .catch(e => console.error(e));
+
+  const warning = () =>
+    log('hello from nosy logger warining example!')
+    .then(() => console.log('logged warning', 'warn'))
+    .catch(e => console.error(e));
+
+  const error = () =>
+    log('hello from nosy logger error example!')
+      .then(() => console.log('logged error', 'error'))
       .catch(e => console.error(e));
 
+
   return (
-    <View style={styles.container}>
-      <Text onPress={send}>Press to log message</Text>
-    </View>
-  );
+      <View style={styles.container}>
+      <Text onPress={debug}>Press to log debug message</Text>
+      <Text onPress={info}>Press to log info message</Text>
+      <Text onPress={warning}>Press to log warning message</Text>
+      <Text onPress={error}>Press to log error message</Text>
+      </View>
+      );
 }
 
 const styles = StyleSheet.create({
