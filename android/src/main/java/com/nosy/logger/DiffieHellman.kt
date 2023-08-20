05770b9654f0b14ee9dfec446295960358d61c6e
Krzysztof Kosobudzki <krzysztof@koia.consulting>
2023-08-20 19:34:30 +0200

    feat(encrypt): do not wrap key

--- android/src/main/java/com/nosy/logger/DiffieHellman.kt
+++ android/src/main/java/com/nosy/logger/DiffieHellman.kt
@@ -1,6 +1,5 @@
 package com.nosy.logger
 
-import android.security.keystore.KeyProperties
 import java.security.KeyFactory
 import java.security.KeyPair
 import java.security.KeyPairGenerator
@@ -14,7 +13,7 @@ import javax.crypto.SecretKey
 internal class DiffieHellman {
 
   private val keyPair: KeyPair by lazy {
-    KeyPairGenerator.getInstance(KEY_PAIR_ALGORITHM, PROVIDER)
+    KeyPairGenerator.getInstance(ALGORITHM, PROVIDER)
       .apply {
         initialize(ECGenParameterSpec(KEY_PAIR_CURVE))
       }
@@ -28,23 +27,22 @@ internal class DiffieHellman {
   }
 
   internal fun sharedSecret(otherPublicKey: String): SecretKey =
-    KeyAgreement.getInstance(KEY_AGREEMENT_ALGORITHM, PROVIDER)
+    KeyAgreement.getInstance(ALGORITHM, PROVIDER)
       .apply {
         init(keyPair.private)
 
         doPhase(otherPublicKey.toPublicKey(), true)
       }
-      .generateSecret(KEY_PAIR_ALGORITHM)
+      .generateSecret(ALGORITHM)
 
   private fun String.toPublicKey(): PublicKey =
     decode()
       .let(::X509EncodedKeySpec)
-      .let(KeyFactory.getInstance(KEY_PAIR_ALGORITHM, PROVIDER)::generatePublic)
+      .let(KeyFactory.getInstance(ALGORITHM, PROVIDER)::generatePublic)
 
   private companion object {
+    private const val ALGORITHM = "ECDH"
     private const val PROVIDER = "BC"
-    private const val KEY_PAIR_ALGORITHM = KeyProperties.KEY_ALGORITHM_EC
     private const val KEY_PAIR_CURVE = "secp256r1"
-    private const val KEY_AGREEMENT_ALGORITHM = "ECDH"
   }
 }
--- android/src/main/java/com/nosy/logger/base64.kt
+++ android/src/main/java/com/nosy/logger/base64.kt
@@ -3,7 +3,7 @@ package com.nosy.logger
 import android.util.Base64
 
 internal fun ByteArray.encode(): String =
-  String(Base64.encode(this, Base64.DEFAULT))
+  Base64.encodeToString(this, Base64.NO_WRAP)
 
 internal fun String.decode(): ByteArray =
   Base64.decode(this, Base64.DEFAULT)
