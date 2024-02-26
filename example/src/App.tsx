da5cb1fa3e5dd53c4d5ca7509cbaa217eb35b7a7
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-02-26 17:50:07 +0100

    chore: apply collector url from properties for both environment

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,44 +1,46 @@
 import React, { useEffect } from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import NosyLogger from 'nosy-logger';
+import NosyLogger from '@nosytools/logger-mobile';
 
 export default function App() {
   useEffect(() => {
-    NosyLogger.init({ apiKey: 'some fake api key' })
+    NosyLogger.init({
+      apiKey:
+        'cIQZXUgpjwQ0d08C5TYg/KD6LHKoFH066093gENxz2yLpGti3C/GBaJlTC4AxGEwwYF/iLFnmRWLE1IVJenGBw==',
+    })
       .then(() => console.log('initialized!'))
       .catch(console.error);
   }, []);
 
   const debug = () =>
     NosyLogger.log('hello from nosy logger debug example!', 'debug')
-    .then(() => console.log('logged debug'))
-    .catch(e => console.error(e));
+      .then(() => console.log('logged debug'))
+      .catch((e) => console.error(e));
 
   const info = () =>
     NosyLogger.log('hello from nosy logger example!')
-    .then(() => console.log('logged info'))
-    .catch(e => console.error(e));
+      .then(() => console.log('logged info'))
+      .catch((e) => console.error(e));
 
   const warning = () =>
     NosyLogger.warning('hello from nosy logger warining example!')
-    .then(() => console.log('logged warning'))
-    .catch(e => console.error(e));
+      .then(() => console.log('logged warning'))
+      .catch((e) => console.error(e));
 
   const error = () =>
     NosyLogger.error('hello from nosy logger error example!')
       .then(() => console.log('logged error'))
-      .catch(e => console.error(e));
-
+      .catch((e) => console.error(e));
 
   return (
-      <View style={styles.container}>
+    <View style={styles.container}>
       <Text onPress={debug}>Press to log debug message</Text>
       <Text onPress={info}>Press to log info message</Text>
       <Text onPress={warning}>Press to log warning message</Text>
       <Text onPress={error}>Press to log error message</Text>
-      </View>
-      );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
