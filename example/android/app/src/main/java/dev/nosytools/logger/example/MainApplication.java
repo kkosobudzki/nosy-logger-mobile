33dc798f2af90fcbfb292e465bff0e352b8a5bab
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-02-26 16:36:11 +0100

    fix: fix package name

--- example/android/app/src/main/java/dev/nosytools/logger/example/MainApplication.java
+++ example/android/app/src/main/java/dev/nosytools/logger/example/MainApplication.java
@@ -1,13 +1,15 @@
 package dev.nosytools.logger.example;
 
 import android.app.Application;
-import dev.facebook.react.PackageList;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+import com.nosy.logger.example.BuildConfig;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
