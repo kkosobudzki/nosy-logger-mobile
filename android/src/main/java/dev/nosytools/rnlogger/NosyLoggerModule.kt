a38810b3ec51a31c09f6b4fe0c01a25f18e62317
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-03-04 12:04:00 +0100

    feat(native): update to use native android library

--- android/src/main/java/dev/nosytools/rnlogger/NosyLoggerModule.kt
+++ android/src/main/java/dev/nosytools/rnlogger/NosyLoggerModule.kt
@@ -1,45 +1,42 @@
 package dev.nosytools.rnlogger
 
-import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.ReadableArray
-import com.facebook.react.bridge.ReadableMap
-import dev.nosytools.rnlogger.Config.Companion.toConfig
 import dev.nosytools.logger.Logger
 
 class NosyLoggerModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
 
-  private lateinit var logger: Logger
+  private val logger by lazy { Logger(reactContext) }
 
   override fun getName(): String {
     return NAME
   }
 
   @ReactMethod
-  fun init(config: ReadableMap, promise: Promise) {
-    Logger(config.toConfig())
-      .run {
-        init(
-          onCompleted = {
-            logger = this
-
-            promise.resolve(true)
-          },
-          onError = promise::reject
-        )
-      }
+  fun init(apiKey: String) {
+    logger.init(apiKey)
   }
 
   @ReactMethod
-  fun log(messages: ReadableArray, promise: Promise) {
-    logger.log(
-      messages.toLogsList(),
-      onCompleted = { promise.resolve(true) },
-      onError = promise::reject
-    )
+  fun debug(message: String) {
+    logger.debug(message)
+  }
+
+  @ReactMethod
+  fun info(message: String) {
+    logger.info(message)
+  }
+
+  @ReactMethod
+  fun warning(message: String) {
+    logger.warning(message)
+  }
+
+  @ReactMethod
+  fun error(message: String) {
+    logger.error(message)
   }
 
   companion object {
