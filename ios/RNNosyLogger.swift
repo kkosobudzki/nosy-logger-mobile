740bfb9ebc0b141ede3e508c6a25eaebf2e5c8b9
Krzysztof Kosobudzki <krzysztof.kosobudzki@gmail.com>
2024-03-08 19:26:22 +0100

    chore: add swift bridge

--- ios/RNNosyLogger.swift
+++ ios/RNNosyLogger.swift
@@ -1,8 +1,32 @@
-@objc(RNNosyLogger)
-class RNNosyLogger: NSObject {
+import NosyLogger
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
+@objc
+class RNNosyLogger: NSObject {
+    
+  private let logger = NosyLogger()
+
+  @objc
+  func start(apiKey: String) -> Void {
+    logger.start(apiKey: apiKey)
+  }
+    
+  @objc
+  func debug(message: String) -> Void {
+    logger.debug(message)
+  }
+    
+  @objc
+  func info(message: String) -> Void {
+    logger.info(message)
+  }
+    
+  @objc
+  func warning(message: String) -> Void {
+    logger.warning(message)
+  }
+    
+  @objc
+  func error(message: String) -> Void {
+    logger.error(message)
   }
 }
